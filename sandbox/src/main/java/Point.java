16d1f3bb0bce42c21e99d06733ec45051906838f
olgamih81 <ola_mi@mail.ru>
2020-07-21 22:38:05 +0300

    Задание 2 (+п.4)
    Доработка задания: преобразование ф-ции в метода в классе Point

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -9,17 +9,23 @@ public class Point {
         this.y = y;
     }
 
-    public static double distance(Point p1, Point p2)
-    {
-        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+    //public static double distance(Point p1, Point p2)
+    //{
+    //    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+//
+ //   }
 
-    }
+    public double distance(Point p2)
+    {
+        return Math.sqrt((p2.x - this.x)*(p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
+
+       }
 
     @Override
     public String toString() {
         return "Point{" +
-                "x=" + x +
-                ", y=" + y +
+                "x=" + this.x +
+                ", y=" + this.y +
                 '}';
     }
 }
