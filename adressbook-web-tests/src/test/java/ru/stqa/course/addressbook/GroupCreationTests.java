4cc6c7b35cc964554915e61bb1084f3c6cb1b318
olgamih81 <ola_mi@mail.ru>
2020-07-27 00:58:10 +0300

    К заданию №4. Инициализация фикстуры
    Выделить из кода теста вспомогательные методы, в том числе метод заполнения формы.

--- adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/GroupCreationTests.java
@@ -12,10 +12,7 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("https://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -24,6 +21,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
