6e61a2bb6456a3a59ae4df209a6b8afe70eda430
olgamih81 <ola_mi@mail.ru>
2020-09-10 09:15:54 +0300

    Подключен Hibernate, установлен коннект с бд

--- adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/model/GroupData.java
+++ adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/model/GroupData.java
@@ -3,18 +3,35 @@ package ru.stqa.course.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
-    @XStreamAlias("group")
+@XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
     @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "group_name")
     private String name;
+
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private String header;
+
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private String footer;
 
     public int getId() {
