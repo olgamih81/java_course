f4867a1cab17b0ab52274a156360d326194b88fb
olgamih81 <ola_mi@mail.ru>
2020-09-17 19:29:08 +0300

    Задание №17: Добавлен модуль для тестов для Mantis(mantis-tst)

---
+++ mantis-tst/src/test/java/ru/stqa/course/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,56 @@
+package ru.stqa.course.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+    private String browser;
+    private final Properties properties;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+     }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+      try {
+        wd.findElement(by);
+        return true;
+      }
+      catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+}
---
+++ mantis-tst/src/test/java/ru/stqa/course/mantis/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.course.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.course.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
