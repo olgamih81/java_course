863b5f688e1bd43b769bd7de79e05429ad853575
olgamih81 <ola_mi@mail.ru>
2020-09-23 23:16:23 +0300

    исправлена ошибка (;)

--- adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
@@ -50,7 +50,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities)
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
 
