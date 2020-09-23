7c003461be0efb2fea3f81d5b8a8b10c69875331
olgamih81 <ola_mi@mail.ru>
2020-09-23 23:35:51 +0300

    ***

--- adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.course.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
