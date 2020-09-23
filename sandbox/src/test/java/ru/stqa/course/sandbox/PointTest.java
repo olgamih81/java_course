b4d1b3c6cf3a963be6b3b5cf3329a668571b51c8
olgamih81 <ola_mi@mail.ru>
2020-09-23 19:20:32 +0300

    испорченый тест 2

--- sandbox/src/test/java/ru/stqa/course/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/course/sandbox/PointTest.java
@@ -12,7 +12,7 @@ public class PointTest {
         Point p1 = new Point(10, 5);
         Point p2 = new Point(3,5);
 
-        Assert.assertEquals(distance(p1, p2), 7);
+        Assert.assertEquals(distance(p1, p2), 10);
         Assert.assertEquals(p1.distance(p2), 7);
     }
 
