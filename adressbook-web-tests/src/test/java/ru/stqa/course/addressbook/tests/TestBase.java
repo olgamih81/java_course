f3e3bcef67a1685321d4c722616d3f9980252152
olgamih81 <ola_mi@mail.ru>
2020-09-28 15:54:48 +0300

    test6

--- adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/TestBase.java
@@ -3,7 +3,7 @@ package ru.stqa.course.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.ITestContext;
+
 import org.testng.annotations.*;
 import ru.stqa.course.addressbook.appmanager.ApplicationManager;
 
@@ -14,7 +14,8 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    protected static final ApplicationManager app = new ApplicationManager(System
+            .getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
